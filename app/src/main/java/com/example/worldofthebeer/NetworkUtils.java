796767ae97d28ec51d8a915eedde54b2295d2afe
Natalia Beckstead <toosmiravilla@gmail.com>
2022-02-10 14:18:37 -0700

    added internet permission, url and parameters

---
+++ app/src/main/java/com/example/worldofthebeer/NetworkUtils.java
@@ -0,0 +1,23 @@
+package com.example.worldofthebeer;
+
+public class NetworkUtils {
+
+    public static String BEER_BASE_URL = "https://api.punkapi.com/v2/beers";
+
+    final static String ABV_GT = "abv_gt";
+    final static String ABV_LT = "abv_lt";
+    final static String IBU_GT = "ibu_gt";
+    final static String IBU_LT = "ibu_lt";
+    final static String EBC_GT = "ebc_gt";
+    final static String EBC_LT = "ebc_lt";
+    final static String BEER_NAME = "beer_name";
+    final static String YEAST = "yeast";
+    final static String BREWED_BEFORE = "brewed_before";
+    final static String BREWED_AFTER = "brewed_after";
+    final static String HOPS = "hops";
+    final static String MALT = "malt";
+    final static String FOOD = "food";
+    final static String IDS = "ids";
+
+
+}
